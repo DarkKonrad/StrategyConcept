7da94bcef00c1f583dde3be14b171361119b3ffc
Konrad Sladkowski <makabrak@gmail.com>
2020-06-14 21:52:59 +0200

    Fixed Algorithm

--- Strategy/Algorithm/Algorithms/QuickSort.h
+++ Strategy/Algorithm/Algorithms/QuickSort.h
@@ -2,6 +2,9 @@
 #include "../Interface/Concepts/RandomAccesIterator.h"
 #include "../Interface/Concepts/Comparable.h"
 
+#include <algorithm>
+#include <iterator>
+
 namespace Algorithm
 {
 	class QuickSort
@@ -17,32 +20,36 @@ namespace Algorithm
 				static Iterator partition(Iterator  begin, Iterator  end, Comparator comparator);
 	};
 
+	
+	template<typename Iterator, typename Comparator> requires Interface::RandomAccessIterator<Iterator>
+		Iterator QuickSort::partition(Iterator  begin, Iterator  end, Comparator comparator)
+		{
+			auto pivot = std::prev(end, 1);
+			auto i = begin;
+
+			for (auto it = begin; it != pivot; ++it)
+			{
+				if (comparator(*it, *pivot))
+				{
+					std::swap(*i++, *it);
+				}
+			}
+			std::swap(*i, *pivot);
+			return i;
+		}
+	
+	
 	template<typename Iterator, typename Comparator> requires Interface::RandomAccessIterator<Iterator> 
 	void QuickSort::sort(Iterator  begin, Iterator  end, Comparator comparator)
 	{
 		if (std::distance(begin,end)>1)
 		{
-			auto boundry = partition(begin, end, comparator);
-			sort(begin, end, comparator);
+			Iterator boundry = partition(begin, end, comparator);
+			sort(begin, boundry, comparator);
 			sort(boundry + 1, end, comparator);
 		}
 
 	}
 
-	template<typename Iterator, typename Comparator> requires Interface::RandomAccessIterator<Iterator> 
-    Iterator QuickSort::partition(Iterator  begin, Iterator  end, Comparator comparator)
-	{
-		auto pivot = std::prev(end, 1);
-		auto i = begin;
 
-		for (auto it = begin; it != pivot; it++)
-		{
-			if (comparator(*it, *pivot))
-			{
-				std::swap(*i++, *it);
-			}
-		}
-		std::swap(*i, *pivot);
-		return i;
-	}
 }
